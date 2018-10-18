55cb87db826400fd8d05d4706527ba96c9d41e51
ariel112 <selvinmorazan@gmail.com>
2018-10-18 10:55:14 -0600

    cambio notificacion

--- EDUSISTEM/app/Notifications/MyResetPassword.php
+++ EDUSISTEM/app/Notifications/MyResetPassword.php
@@ -39,12 +39,15 @@ class MyResetPassword extends Notification
      * @param  mixed  $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
-    public function toMail($notifiable)
+     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+            ->subject('Recuperar contraseña')
+            ->greeting('Hola')
+            ->line('Estás recibiendo este correo porque hiciste una solicitud de recuperación de contraseña para tu cuenta.')
+            ->action('Recuperar contraseña', route('password.reset', $this->token))
+            ->line('Si no realizaste esta solicitud, no se requiere realizar ninguna otra acción.')
+            ->salutation('Saludos, '. config('app.name'));
     }
 
     /**
